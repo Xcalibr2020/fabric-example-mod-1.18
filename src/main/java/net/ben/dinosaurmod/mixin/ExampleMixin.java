ce1cd6916418b95c8dda69053a2c609330875ac3
Ben <benjamincoon2020@gmail.com>
2022-12-01 12:06:40 -0700

    second try to add code to github

---
+++ src/main/java/net/ben/dinosaurmod/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.ben.dinosaurmod.mixin;
+
+import net.ben.dinosaurmod.dinosaurmod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		dinosaurmod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
